574b9f2d9b91759e0a558bcaa0ef316fb1208904
Roomsie Jones <seangravito@gmail.com>
2021-07-13 16:46:13 -0500

    added more tests to Employee.test.js and ran it

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,35 @@
-const { expect } = require('@jest/globals');
+const { expect, it } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 describe('Employee class', () =>{
     it('Should return a name', () => {
-        const employee = new Employee('Jonathon');
+        const employee = new Employee('Jonathon', 32423, 'jon@gmail.com');
 
         expect(employee.name).toEqual('Jonathon');
+    })
+
+    it('should return the employee id', () => {
+        const employee = new Employee('Jonathon', 32423, 'jon@gmail.com');
+
+        expect(employee.id).toEqual(32423);
+    })
+
+    it('should return an email address', () => {
+        const employee = new Employee('Jonathon', 32423, 'jon@gmail.com');
+
+        expect(employee.email).toEqual('jon@gmail.com');
+    })
+
+    it('should return the employee name', () => {
+        const employee = new Employee('Jonathon', 32423, 'jon@gmail.com');
+
+        expect(employee.getName()).toBe('Jonathon');
+    })
+    
+    it('should return the employee id', () => {
+        const employee = new Employee('Jonathon', 32423, 'jon@gmail.com');
+
+        expect(employee.getId()).toBe(32423);
     })
 
     it('should return an email when using getemail', () => {
