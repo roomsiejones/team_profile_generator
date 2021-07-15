ec9a0243cd2cd2072ad8ff8764e5f96c66917948
Roomsie Jones <seangravito@gmail.com>
2021-07-14 19:27:43 -0500

    created an initial file where Ill be doing the inquirer promptsand holding the list of employees - created the class constructor to hold them

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+
+const Manager = require('');
+const Engineer = require('');
+const Intern = require('');
+const HTMLCreate = require('');
+
+class App {
+    constructor() {
+        this.employees = [];
+    }
+
+
+}
